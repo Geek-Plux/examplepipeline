e6ad404e1342b08cf18d533674640e38932986b0
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2020-10-12 06:01:51 +0000

    Todo update

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,4 +2,11 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
-     
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'chore',
+        's
