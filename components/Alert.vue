ee8fe51c4ef4071a260ee351f1601acc5492396f
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2020-10-28 13:41:00 +0000

    FIXME

---
+++ components/Alert.vue
@@ -0,0 +1,32 @@
+
+<script lang="ts" setup>
+import { TransitionRoot, TransitionChild } from '@headlessui/vue'
+export type IStyles = 'primary' | 'success' | 'warning' | 'danger'
+
+// props
+const props = defineProps({
+  title: {
+    type: String,
+    default: undefined,
+  },
+  text: {
+    type: String,
+    default: undefined,
+  },
+  type: {
+    type: String,
+    default: 'primary',
+  },
+})
+
+// styles
+const styles = reactive<{
+  [key: string]: string
+}>({
+  primary: '',
+  success:
+    'dark:from-green-500/50 via-gray-200 to-gray-200 dark:via-slate-800 dark:to-slate-800',
+  warning:
+    'dark:from-yellow-500/50 via-gray-200 to-gray-200 dark:via-slate-800 dark:to-slate-800',
+  danger:
+    'dark:from-red-500/50 via-gray-200 to-gray-200 dark:via-slate-800 dark:to-slate-800',
