8232593cfc60669bf105dbe622fe62f562d1dc51
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2020-10-28 17:18:08 +0000

    Fix MSVC build

---
+++ components/Anchor.vue
@@ -0,0 +1,17 @@
+
+<script lang="ts" setup>
+// micro compiler
+const props = defineProps({
+  text: {
+    type: String,
+    default: '',
+  },
+  to: {
+    type: [String, Object],
+    default: undefined,
+  },
+  href: {
+    type: String,
+    default: '',
+  },
+})
