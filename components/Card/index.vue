433776f0b7178582811820569a36f8345df07e41
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2020-12-03 17:43:42 +0000

    removed old commented code

--- components/Card/index.vue
+++ components/Card/index.vue
@@ -9,4 +9,8 @@ defineProps({
 
 <template>
   <div
-    class="card duration-300 transition-colors w-full relative rounded overflow-hidden bg-white dark:bg-slate-900 border border-gray-900/10 da
+    class="card duration-300 transition-colors w-full relative rounded overflow-hidden bg-white dark:bg-slate-900 border border-gray-900/10 dark:border-gray-50/[0.2]"
+  >
+    <div
+      v-if="disabled"
+      class="absolute z-10 top-0 lef
