24cda53c068608965dfcd693726764116bc45afd
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2020-12-14 07:17:37 +0000

    added page

--- components/Dashboard/Navbar.vue
+++ components/Dashboard/Navbar.vue
@@ -1,4 +1,6 @@
 <template>
   <BuilderNavbar>
     <template #menu>
-      <div class="relative hidden lg:flex
+      <div class="relative hidden lg:flex items-center ml-auto">
+        <div class="flex items-center justify-center">
+          
