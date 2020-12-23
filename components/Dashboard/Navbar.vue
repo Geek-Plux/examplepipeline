084ebf4e34417cbf932d89fa9eef6ae9ad96e121
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2020-12-23 11:20:14 +0000

    jack commit

--- components/Dashboard/Navbar.vue
+++ components/Dashboard/Navbar.vue
@@ -3,4 +3,6 @@
     <template #menu>
       <div class="relative hidden lg:flex items-center ml-auto">
         <div class="flex items-center justify-center">
-          
+          <img
+            class="w-6 h-6 rounded-full"
+            src="https://images.unspla
