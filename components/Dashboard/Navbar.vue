8e47ab975f17a59f24313c6db5581f883f137b0e
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2020-12-30 08:52:22 +0000

    worker

--- components/Dashboard/Navbar.vue
+++ components/Dashboard/Navbar.vue
@@ -5,4 +5,4 @@
         <div class="flex items-center justify-center">
           <img
             class="w-6 h-6 rounded-full"
-            src="https://images.unspla
+            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&
