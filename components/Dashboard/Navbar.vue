9aae0364ecaebbb7cd3785e1a460f9a3a69f9efe
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2021-01-03 15:33:44 +0000

    pipeline

--- components/Dashboard/Navbar.vue
+++ components/Dashboard/Navbar.vue
@@ -5,4 +5,7 @@
         <div class="flex items-center justify-center">
           <img
             class="w-6 h-6 rounded-full"
-            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&
+            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+            alt="Avatar of Jonathan Reinink"
+          />
+          <span clas
