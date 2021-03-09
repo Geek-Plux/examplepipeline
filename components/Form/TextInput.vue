8cb0acdaaffd25e4de586e5650426512a4f1dceb
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2021-03-09 15:03:20 +0000

    PCI: Fix infinite loop with ROM image of size 0

--- components/Form/TextInput.vue
+++ components/Form/TextInput.vue
@@ -65,4 +65,9 @@ const selectedFontSizeStyle = computed(
   <div :class="`text-input-container relative flex`">
     <div
       v-if="slots['prefix-disabled']"
-      :class="`flex rounded-l bg-gray-100 dark:bg-slate-800 text-gray-500 border ${selectedBorde
+      :class="`flex rounded-l bg-gray-100 dark:bg-slate-800 text-gray-500 border ${selectedBorderStyle}`"
+    >
+      <slot name="prefix-disabled" />
+    </div>
+    <div
+      v-if="
