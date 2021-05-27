7aa9c05e9bfcab2046fc17f524e9e36fea3e8edc
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2021-05-27 22:05:28 +0000

    Minor documentation updates.

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -40,4 +40,9 @@ const localeSetting = useState<string>('locale.setting')
         </span>
       </ListboxButton>
       <ListboxOptions
-        class="p-1 absolute z-50 top-full right-0 outline-none bg-white rounded-lg ring-1 ring-gray-900/10 shadow-lg overflow-hidden w-36 py-1 text-sm text-gray-700 font-semibold dark:bg-gray-800 dark:ring-0 dark:highlight-white/5 dark:text-gray-300
+        class="p-1 absolute z-50 top-full right-0 outline-none bg-white rounded-lg ring-1 ring-gray-900/10 shadow-lg overflow-hidden w-36 py-1 text-sm text-gray-700 font-semibold dark:bg-gray-800 dark:ring-0 dark:highlight-white/5 dark:text-gray-300"
+      >
+        <ListboxOption
+          v-for="lang in availableLocales"
+          :key="lang.iso"
+          :valu
