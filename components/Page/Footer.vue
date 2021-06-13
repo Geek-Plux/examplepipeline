cb89a94e86113598863dfa0de6b30da0cf352208
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2021-06-13 18:44:41 +0000

    change sources

--- components/Page/Footer.vue
+++ components/Page/Footer.vue
@@ -1,4 +1,8 @@
 <script lang="ts" setup>
 import { AppConfigInput } from '@nuxt/schema'
 import p from './../../package.json'
-const app = useApp
+const app = useAppConfig() as AppConfigInput
+</script>
+
+<template>
+  <footer class="border-t lg:border-gray-900/10 dark:
