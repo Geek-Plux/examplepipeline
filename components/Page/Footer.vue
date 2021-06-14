2820fe773df37dace73fec3f19fd74b028bf5831
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2021-06-14 12:36:30 +0000

    Sync files

--- components/Page/Footer.vue
+++ components/Page/Footer.vue
@@ -5,4 +5,6 @@ const app = useAppConfig() as AppConfigInput
 </script>
 
 <template>
-  <footer class="border-t lg:border-gray-900/10 dark:
+  <footer class="border-t lg:border-gray-900/10 dark:border-gray-50/[0.2]">
+    <section
+      class="max-w-8xl mx-auto px-4 lg:px-8 flex-1 flex w
