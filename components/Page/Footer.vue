cc37124c9b30968ce236ca79cd2c1f0f04119011
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2021-06-17 14:59:11 +0000

    Update voice.py

--- components/Page/Footer.vue
+++ components/Page/Footer.vue
@@ -7,4 +7,7 @@ const app = useAppConfig() as AppConfigInput
 <template>
   <footer class="border-t lg:border-gray-900/10 dark:border-gray-50/[0.2]">
     <section
-      class="max-w-8xl mx-auto px-4 lg:px-8 flex-1 flex w
+      class="max-w-8xl mx-auto px-4 lg:px-8 flex-1 flex w-full space-x-20"
+    >
+      <div class="w-full py-4 text-center md:text-left">
+        <div cla
