392a2ffd354d32718b686da5f0213b91ef158b13
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2021-09-19 10:18:10 +0000

    Update abruzzomap.html

--- global.d.ts
+++ global.d.ts
@@ -6,4 +6,12 @@ import { ModuleOptions as NuxtContentModuleOptions } from '@nuxt/content'
 declare module '@nuxt/schema' {
   interface AppConfigInput {
     name: string
-  
+    author: {
+      name: string
+      link: string
+    }
+  }
+}
+
+declare module 'nuxt/config' {
+  inte
