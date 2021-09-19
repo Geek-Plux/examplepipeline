2b6703e3006829c97688383c8b9ea3a3f984cdc3
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2021-09-19 09:17:32 +0000

    Updated changelogs

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,9 @@
 import { IntlifyModuleOptions } from '@intlify/nuxt3'
 import { VueUseNuxtOptions } from '@vueuse/nuxt'
 import { ModuleOptions as NuxtWindiCssModuleOptions } from 'nuxt-windicss'
-import { ModuleOptions as NuxtContentModuleOptio
+import { ModuleOptions as NuxtContentModuleOptions } from '@nuxt/content'
+
+declare module '@nuxt/schema' {
+  interface AppConfigInput {
+    name: string
+  
