c9bf6fadb00e3f0a9309a4b4037a333436eac1ad
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2021-09-20 07:45:08 +0000

    Updated submodules.

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,4 +1,7 @@
 <template>
   <div>
     <slot name="app-before" />
-    <div id="app-before"><
+    <div id="app-before"></div>
+    <div class="flex flex-col min-h-screen">
+      <slot name="header">
+        <DashboardNavb
