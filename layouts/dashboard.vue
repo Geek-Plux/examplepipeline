fb247d80ebd117ba4a048f79b5e5dee5c0b91bf4
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2021-09-24 13:34:31 +0000

    [Releaser] Bump new snapshot version

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -12,4 +12,6 @@
       </slot>
       <div class="flex-1 w-full flex flex-col">
         <div
-  
+          class="relative flex-1 flex flex-row mx-auto max-w-8xl w-full h-full"
+        >
+          
