fc314c4cac357c74914c21db8652b99e1aa198e9
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2021-11-14 15:56:58 +0000

    - merge with edhelas

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,3 +38,68 @@ export default defineNuxtConfig({
   experimental: {
     reactivityTransform: false,
   },
+
+  // auto import components
+  components: true,
+
+  // vite plugins
+  vite: {
+    plugins: [
+      UnpluginComponentsVite({
+        dts: true,
+        resolvers: [
+          IconsResolver({
+            prefix: 'Icon',
+          }),
+        ],
+      }),
+    ],
+  },
+
+  // app config
+  app: {
+    // global transition
+    pageTransition: { name: 'page', mode: 'out-in' },
+    layoutTransition: { name: 'layout', mode: 'out-in' },
+  },
+
+  // localization - i18n config
+  intlify: {
+    localeDir: 'locales',
+    vueI18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      availableLocales: ['en', 'id', 'ja', 'ko'],
+    },
+  },
+
+  // vueuse
+  vueuse: {
+    ssrHandlers: true,
+  },
+
+  // windicss
+  windicss: {
+    analyze: {
+      analysis: {
+        interpretUtilities: false,
+      },
+      server: {
+        port: 4000,
+        open: false,
+      },
+    },
+    scan: true,
+  },
+
+  // content
+  content: {
+    documentDriven: true,
+    markdown: {
+      mdc: true,
+    },
+    highlight: {
+      theme: 'github-dark',
+    },
+  },
+})
