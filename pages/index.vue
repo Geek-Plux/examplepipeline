067bb5e64e87016abb3944c91dd9927e86b25fc8
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2022-02-12 12:11:57 +0000

    Add user

--- pages/index.vue
+++ pages/index.vue
@@ -56,4 +56,6 @@ const copyBash = () => {
         class="absolute hidden md:block top-0 left-0 transform translate-x-18 translate-y-20 h-28 w-28 rounded-full bg-blue-600 linear-wipe"
       ></div>
       <div
-        class="absolute hidden md:block bottom-0 right-0 transform -translate-x-4 -translate-y-40 
+        class="absolute hidden md:block bottom-0 right-0 transform -translate-x-4 -translate-y-40 h-16 w-16 rounded bg-purple-600 linear-wipe"
+      ></div>
+      <div class="absolute bottom-0 ri
