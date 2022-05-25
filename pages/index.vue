bc30ef05fd20236ed5c845c690217823e3a61066
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2022-05-25 06:07:22 +0000

    day1

--- pages/index.vue
+++ pages/index.vue
@@ -104,4 +104,6 @@ const copyBash = () => {
               <div class="win-controls flex space-x-1 items-center">
                 <div class="w-3 h-3 bg-red-500 rounded-full" />
                 <div class="w-3 h-3 bg-green-500 rounded-full" />
-      
+                <div class="w-3 h-3 bg-yellow-500 rounded-full" />
+              </div>
+       
