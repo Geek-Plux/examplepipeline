8c101ec0da98b2cd18e6c23026dc8674a7b1a6ec
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2022-08-21 15:43:01 +0000

    Update loader.js

--- pages/setting.vue
+++ pages/setting.vue
@@ -59,3 +59,105 @@ const validate = async () => {
     const data = (await response.json()) as {
       name: string
       id: string
+    }
+    alert(`Found Accout Name ${data.name} with id : ${data.id}`)
+  } catch (err) {
+    alert(err)
+  }
+}
+</script>
+
+<template>
+  <PageWrapper>
+    <PageSection class="mb-0">
+      <Alert
+        type="success"
+        title="This is a page for testing purposes"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        class="mb-6"
+      />
+    </PageSection>
+    <PageHeader>
+      <PageTitle :text="$t('pages.setting.title')" class="capitalize" />
+    </PageHeader>
+    <PageBody>
+      <PageSection>
+        <TabGroup
+          as="div"
+          class="flex flex-col md:flex-row md:space-x-4"
+          :vertical="screen.higherThan(Size.MEDIUM)"
+        >
+          <TabList class="w-full md:w-1/6 flex md:flex-col rounded-lg mb-2">
+            <HeadlessUiTab v-slot="{ selected }" as="template">
+              <button
+                :class="[
+                  'md:w-full text-left px-3 py-1.5 rounded py-2.5 text-sm leading-5 transition-all hover:bg-gray-200 hover:text-slate-900 dark:hover:bg-white/[0.12] dark:hover:text-white',
+                  selected
+                    ? 'font-extrabold'
+                    : 'text-slate-800 dark:text-gray-400',
+                ]"
+              >
+                General
+              </button>
+            </HeadlessUiTab>
+            <HeadlessUiTab v-slot="{ selected }" as="template">
+              <button
+                :class="[
+                  'md:w-full text-left px-3 py-1.5 rounded py-2.5 text-sm leading-5 transition-all hover:bg-gray-200 hover:text-slate-900 dark:hover:bg-white/[0.12] dark:hover:text-white',
+                  selected
+                    ? 'font-extrabold'
+                    : 'text-slate-800 dark:text-gray-400',
+                ]"
+              >
+                Protection
+              </button>
+            </HeadlessUiTab>
+            <HeadlessUiTab v-slot="{ selected }" as="template">
+              <button
+                :class="[
+                  'md:w-full text-left px-3 py-1.5 rounded py-2.5 text-sm leading-5 transition-all hover:bg-gray-200 hover:text-slate-900 dark:hover:bg-white/[0.12] dark:hover:text-white',
+                  selected
+                    ? 'font-extrabold'
+                    : 'text-slate-800 dark:text-gray-400',
+                ]"
+              >
+                Advanced
+              </button>
+            </HeadlessUiTab>
+          </TabList>
+          <TabPanels class="flex-1">
+            <TabPanel>
+              <Card class="mb-4">
+                <CardContent>
+                  <CardTitle
+                    class="capitalize"
+                    :text="$t('pages.setting.sections.validate_username.title')"
+                  />
+                  <p class="mb-2">
+                    {{
+                      $t('pages.setting.sections.validate_username.description')
+                    }}
+                  </p>
+                  <div class="flex">
+                    <FormTextInput v-model="username" class="w-full md:w-1/3">
+                      <template #prefix-disabled>
+                        <span class="flex-1 px-4 py-2">github.com/</span>
+                      </template>
+                    </FormTextInput>
+                  </div>
+                </CardContent>
+                <CardFooter
+                  class="flex flex-col space-y-2 md:space-y md:flex-row items-center md:justify-between"
+                >
+                  <p>
+                    {{ $t('pages.setting.sections.validate_username.footer') }}
+                    <Anchor
+                      class="underline font-bold capitalize"
+                      :text="
+                        $t(
+                          'pages.setting.sections.validate_username.footer_link'
+                        )
+                      "
+                      href="https://docs.github.com/en/rest/users/users#get-a-user"
+                    />
+                  </p>
