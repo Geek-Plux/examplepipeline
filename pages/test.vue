701623f9c9e98d8bce553dce081a4cfa3af02335
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2022-09-04 07:37:43 +0000

    tracepoint: Do not waste memory on mods with no tracepoints

--- pages/test.vue
+++ pages/test.vue
@@ -43,3 +43,65 @@ const identity = useIdentity()
               type="secondary"
               size="sm"
               :text="$t('pages.test.increment')"
+              @click.prevent="counter.increment"
+            />
+            <Button
+              class="w-full md:w-auto"
+              type="secondary"
+              size="sm"
+              :text="`${$t('pages.test.increment')} 2x`"
+              @click.prevent="counter.increment2x"
+            />
+            <Button
+              class="w-full md:w-auto capitalize"
+              type="secondary"
+              size="sm"
+              :text="$t('pages.test.decrement')"
+              @click.prevent="counter.decrement"
+            />
+            <Button
+              class="w-full md:w-auto capitalize"
+              type="secondary"
+              size="sm"
+              :text="$t('pages.test.reset')"
+              @click.prevent="counter.reset"
+            />
+          </div>
+        </div>
+      </PageSection>
+      <PageSection>
+        <PageSectionTitle
+          :text="$t('pages.test.identity')"
+          class="capitalize"
+        />
+        <div class="mb-2">
+          <span class="capitalize">{{ $t('pages.test.full_name') }} : </span>
+          <span>{{ identity.fullName }}</span>
+        </div>
+        <div class="mb-2">
+          <div
+            class="flex flex-col items-center space-y-2 md:space-y-0 md:flex-row md:space-x-2"
+          >
+            <FormTextInput
+              v-model="identity.firstName"
+              size="md"
+              class="w-full md:w-1/3"
+            />
+            <FormTextInput
+              v-model="identity.lastName"
+              size="md"
+              class="w-full md:w-1/3"
+            />
+            <Button
+              class="capitalize w-full md:w-auto"
+              :text="$t('pages.test.reset')"
+              type="secondary"
+              size="md"
+              @click.prevent="identity.reset"
+            />
+          </div>
+        </div>
+      </PageSection>
+    </PageBody>
+  </PageWrapper>
+</template>
