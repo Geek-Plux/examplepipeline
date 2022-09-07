93a224ef7d26fea727df0b5ad550bea5e3497c7f
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2022-09-07 18:04:06 +0000

    Introduction

--- plugins/navbar.ts
+++ plugins/navbar.ts
@@ -2,4 +2,6 @@ export default defineNuxtPlugin((nuxtApp) => {
   // when page redirect on mobile device, close drawer navbar
   nuxtApp.hook('page:finish', () => {
     const showDrawer = useState<boolean>('navbar.showDrawer', () => false)
-    const showOptions = useSta
+    const showOptions = useState<boolean>('navbar.showOptions', () => false)
+    showDrawer.value = false
+    showOptions.value 
