4ed070aed475182707396cb7cdf6dc9be68770ef
Geek-Plux <110288247+Geek-Plux@users.noreply.github.com>
2023-01-18 15:21:46 +0000

    Update jccoe.md

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -2,4 +2,7 @@ import { fileURLToPath } from 'node:url'
 import { describe, expect, it } from 'vitest'
 import { setup, $fetch, isDev } from '@nuxt/test-utils-edge'
 
-describe('exam
+describe('example', async () => {
+  await setup({
+    rootDir: fileURLToPath(new URL('..', import.meta.url)),
+    serv
